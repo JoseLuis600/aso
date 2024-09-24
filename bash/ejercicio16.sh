3079d08bd22edb4d8fc3a32ac32de8d527112e62
JoseLuis600 <josjuamol@alu.edu.gva.es>
2024-09-24 10:03:15 +0200

    Modificado ejercicio 16

--- bash/ejercicio16.sh
+++ bash/ejercicio16.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
 
-for (( i=1; i<=1000; i++)); do
-  echo $i
-done
+echo "1. for"
+echo "2. while"
+echo "3. until"
+read -p "Eleccion: " opcion
 
-valor = 1
-while [[ $valor -ne 1000 ]]; do
-  suma=$((valor + 1))
-  echo $suma
-done
+case $opcion in
+
+  1)
+    for (( i=1; i<=1000; i++)); do
+      echo $i
+      if [[ $i -eq 1000 ]]; then
+        echo "For"
+      fi
+    done
+  ;;
+
+  2)
+    valor=1
+    while [[ $valor -ne 1000 ]]; do
+      suma=$((valor + 1))
+      valor=$suma
+      echo $suma
+      if [[ $valor -eq 1000 ]]; then
+        echo "While"
+      fi
+    done
+  ;;
+
+  3)
+    valor=1
+    until [[ $valor -eq 1000 ]]; do
+      suma=$((valor + 1))
+      valor=$suma
+      echo $suma
+      if [[ $valor -eq 1000 ]]; then
+        echo "Until"
+      fi
+    done
+  ;;
+esac
