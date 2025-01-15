4e29e33b5e4d51ad7fed8b25394f760f11660730
JoseLuis600 <josjuamol@alu.edu.gva.es>
2025-01-15 09:44:14 +0100

    Añadido AD-DC y completado Practica 3

---
+++ AD-DC/Practica3/Actividad2.ps1
@@ -0,0 +1,16 @@
+﻿$departamentos = Import-Csv departamentos.csv -Delimiter ";"
+$usuarios = Import-Csv empleados.csv -Delimiter ";"
+
+#Crea la unidad Empresa
+New-ADOrganizationalUnit -Name "Empresa" -Path "DC=ASO,DC=LOCAL" -Description "Unidad Empresa"
+
+#Ambos foreach se encargan de crear las unidades, los grupos, los usuarios y asigna los usuarios a sus respectivos grupos
+foreach ($dep in $departamentos) {
+    New-ADOrganizationalUnit -Name $dep.departamento -Path "OU=Empresa,DC=ASO,DC=LOCAL" -Description $dep.descripcion
+    New-ADGroup -Name $dep.departamento -GroupCategory Security -GroupScope Global -Path "OU=$($dep.departamento),OU=Empresa,DC=ASO,DC=LOCAL"
+}
+
+foreach ($emp in $usuarios) {
+    New-ADUser -Name "$($emp.nombre) $($emp.apellido)" -Path "OU=$($emp.departamento),OU=Empresa,DC=ASO,DC=LOCAL" -AccountPassword (ConvertTo-SecureString "hola01?" -AsPlainText -Force) -GivenName $emp.nombre -Surname $emp.apellido -ChangePasswordAtLogon $false -Enabled $true
+    Add-ADGroupMember -Identity $emp.departamento -Members "$($emp.nombre) $($emp.apellido)"
+}
