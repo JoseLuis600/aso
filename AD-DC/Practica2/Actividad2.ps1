57cc45adfdb645a5ab677b92a6416b707fede43a
JoseLuis600 <josjuamol@alu.edu.gva.es>
2025-01-29 09:46:06 +0100

    Subido Practica2

---
+++ AD-DC/Practica2/Actividad2.ps1
@@ -0,0 +1,23 @@
+﻿$departamentos = Import-Csv departamentos.csv -Delimiter ";"
+
+foreach ($dep in $departamentos) {
+New-Item -ItemType Directory -Path C:\Empresa\$($dep.departamento)
+}
+
+New-SmbShare -Path C:\Empresa -name empresa
+Grant-SmbShareAccess -name empresa -AccountName Administradores -AccessRight Full -force
+
+foreach ($dep in $departamentos) {
+$acl = Get-Acl -Path C:\Empresa\$($dep.departamento)
+$acl.SetAccessRuleProtection($true, $false)
+$permisoadmin = @('Administradores', 'FullControl', 'ContainerInherit, ObjectInherit', 'None', 'Allow')
+$permisodep = @($dep.departamento, 'Modify', 'ContainerInherit, ObjectInherit', 'None', 'Allow')
+$permisoall = $permisodep = @('Usuarios', 'Read', 'ContainerInherit, ObjectInherit', 'None', 'Allow')
+$reglaadmin = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permisoadmin
+$regladep = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permisodep
+$reglaall = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permisoall
+$acl.SetAccessRule($reglaadmin)
+$acl.SetAccessRule($regladep)
+$acl.SetAccessRule($reglaall)
+$acl | Set-Acl -Path C:\Empresa\$($dep.departamento)
+}
