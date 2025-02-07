3c08c374a65bdcd0d03c9d2a7801e252f7cbd8cc
JoseLuis600 <josjuamol@alu.edu.gva.es>
2025-02-07 08:37:31 +0100

    Subido Ejercicio3 y Creada la carpeta Procesos

---
+++ Procesos/Practica1/Ejercicio3.ps1
@@ -0,0 +1,13 @@
+﻿Write-Host "Script para detener procesos"
+$consulta = Read-Host "Escriba el usuario: "
+
+$encontrar = Get-Process -IncludeUserName | Where UserName -eq "ASO\$consulta"
+
+if ($encontrar) {
+    foreach ($enc in $encontrar) {
+        Stop-Process -Id $enc.Id -Force
+    }
+    Write-host "Procesos eliminados"
+} else {
+    Write-Host "No se encontraron procesos"
+}
